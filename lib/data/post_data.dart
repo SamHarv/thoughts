6b2fdcb42e0b48708499ef90de34dd7e0b100d53
Sam Harvey <srharvey93@gmail.com>
2025-03-25 10:44:19 +1100

    Redraft

--- lib/data/post_data.dart
+++ lib/data/post_data.dart
@@ -7,12 +7,12 @@ List<Post> postData = [
     datePosted: DateTime(2025, 3, 28),
     image: 'images/1.png',
     id: '171',
-    body: 'We as a species are adaptable by nature.\n\n'
+    body: 'We sapiens are adaptable by nature.\n\n'
         'This is why we have evolved and succeeded to such an extent.\n\n'
-        'We are now in a time where we often do not need to be as adaptable.\n\n'
+        'We exist in a time where we often do not need to be as adaptable.\n\n'
         'We have the technology to change the world around us rather than change '
         'ourselves.\n\n'
-        'I believe this is dangerous and can lead us to not handle change well.\n\n'
+        'I believe this is dangerous and can lead to us handling change poorly.\n\n'
         'Change is inevitable and not everything can be controlled.\n\n'
         'Change what you can and accept and adapt to what you cannot.\n\n'
         'If you do not bend, you will break.\n\n'
