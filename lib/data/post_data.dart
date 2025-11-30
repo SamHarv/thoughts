6f9965269a0f0ba7b53b95693f4253ab79157c40
Sam Harvey <srharvey93@gmail.com>
2025-12-01 08:48:54 +1100

    New draft

--- lib/data/post_data.dart
+++ lib/data/post_data.dart
@@ -6,14 +6,23 @@ import 'models/post_model.dart';
 
 List<Post> postData = [
   Post(
+    title: 'Character',
+    datePosted: DateTime(2025, 12, 5),
+    image: 'images/1.png',
+    id: '207',
+    body:
+        '"The Person of honour thinks of their character, the inferior person '
+        'of their position" - Confucius',
+  ),
+  Post(
     title: 'Onward',
     datePosted: DateTime(2025, 11, 28),
     image: 'images/1.png',
     id: '206',
-    body: 'Some spend so much time trying to recreate old experiences that '
-        'they miss out on creating new ones.\n\n'
-        'We should often keep fond memories to be just that, memories.\n\n'
-        'This way we increase our chances of finding even greater experiences.',
+    body: 'Some spend so much time trying to recreate old experiences '
+        'they miss out on new ones.\n\n'
+        'We should keep fond memories to be just that, memories.\n\n'
+        'This way we increase our chances of greater experiences.',
   ),
   Post(
     title: 'Adapt',
